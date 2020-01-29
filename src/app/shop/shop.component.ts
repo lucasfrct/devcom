b31949ed001cc07c4253322c8dcfe135ac03b8f7
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-01-29 18:34:07 -0300

    Add Purchase in Shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -10,14 +10,12 @@ declare var M: any
 
 export class ShopComponent implements OnInit {
     
-    private uid: any;
-    private counter = 0;
-    private observers = [];
-    
+    private uid: any
+
     public control = {
         modal: null,
-        total: 0,
-    };
+        total: 0,   
+    }
     
     public ticket = {
         owner: "",
@@ -25,89 +23,121 @@ export class ShopComponent implements OnInit {
         seat: { session: "", type: "" },
         edition: {},
         price: "0"
-    };
-    
-    public purchase = [];
-    
-    public event = {
-        name: "Castelos dos Filhos das Ruas",
-        folderUrl: "assets/images/sources/art-1.jpeg",
-        logoUrl: "assets/images/sources/logo-1.jpeg",
-        date: "08 de Fev de 2020",
-        hour: "20h00",
-        address: "Cine Atlantico",
     }
     
-    constructor() {
+    public purchase = []
+    
+    public event = {}
+    
+    public constructor() {
+        //this.uid = user.uid
     }
     
-    ngOnInit() { }
+    ngOnInit() { 
+        this.event = this.getEvent()
+    }
     
     public onChange() {
         this.ticket.price = (this.ticket.seat.type == "VIP") ? "5000" : "3000";
     }
     
     public addTicked() {
-        if (this.validate(this.ticket).check) {
-            this.control.modal = true;
-            this.ticket.event = this.event;
-            this.getTicketEdition((edition) => {
-                this.control.modal = false;
-                this.ticket.edition = edition;
-                this.purchase.push(this.copy(this.ticket));
-                this.calTotal(this.purchase);
-                M.toast({ html: 'Ingresso adicionado' });
+
+        var that = this
+        
+        if (that.validate(that.ticket).check) {
+            
+            that.control.modal = true
+            
+            that.ticket.event = that.event
+            
+            that.getTicketEdition((edition) => {
+                
+                that.control.modal = false;
+                that.ticket.edition = edition;
+                
+                that.purchase.push(that.copy(that.ticket))
+
+                that.control.total = that.calcTotal(that.purchase)
+
+                M.toast({ html: 'Ingresso adicionado' })
+
             });
         }
     }
+
+    public deleteTicket(index: any) {
+        this.purchase.splice(index, 1)
+        this.control.total = this.calcTotal(this.purchase)
+    }
+
+    public onDelete(index: any) {
+        this.deleteTicket(index)
+    }
+
+    private getEvent() {
+        return  {
+            name: "Castelos dos Filhos das Ruas",
+            folderUrl: "assets/images/sources/art-1.jpeg",
+            logoUrl: "assets/images/sources/logo-1.jpeg",
+            date: "08 de Fev de 2020",
+            hour: "20h00",
+            address: "Cine Atlantico",
+        }
+    }
     
-    private calTotal(purchase: any) {
-        var that = this;
-        var calc = 0;
-        purchase.forEach((tk) => {
-            calc = (calc + Number(tk.price));
-        });
-        return this.control.total = calc;
+    private calcTotal(purchase: any) {
+        var result = 0;
+        purchase.forEach((ticket) => {
+            if (ticket.price) {
+                result = (result + Number(ticket.price))
+            }
+        })
+        return result;
     }
    
     private getTicketEdition(callback: any) {
-        console.log("GET EDITION");
-        callback({ circulation: "01", serial: "000001" });
+        callback({ circulation: "01", serial: "000001" })
+    }
+
+    public finallyPurchase(purchase) {
+        console.log("Finally: ", purchase)
     }
     
     private validate(ticket: any) {
         var valid = { check: false };
+        
         if (!name(ticket.owner)) {
             M.toast({ html: 'Favor digitar um nome com mais de 3 letras' });
         }
+        
         if (!seatSession(ticket.seat.session)) {
             M.toast({ html: 'Favor Selecionar um assento.' });
         }
+        
         if (!seatType(ticket.seat.type)) {
             M.toast({ html: 'Favor Selecionar um tipo de ingresso.' });
         }
-        valid.check = (name(ticket.owner) && seatSession(ticket.seat.session) && seatType(ticket.seat.type)) ? true : false;
+        
+        valid.check = (
+            name(ticket.owner) 
+            && seatSession(ticket.seat.session) 
+            && seatType(ticket.seat.type)
+        ) ? true : false;
+        
         return valid;
+        
         function name(name: String) {
             return (name.length >= 3) ? true : false;
         }
+        
         function seatSession(session: any) {
             return (session.length >= 3) ? true : false;
         }
+       
         function seatType(type: any) {
             return ("VIP" == type || "Normal" == type) ? true : false;
         }
-    }
-    
-    public Subscribe(command) {
-        this.observers.push(command);
-    }
-    
-    public NotifyAll(command) {
-        this.observers.forEach((ObserverFunstion) => {
-            ObserverFunstion(command);
-        });
-        this.observers = [];
     }
     
     private copy(obj: any) {
