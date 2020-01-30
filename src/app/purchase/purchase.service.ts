8ccce27a84805c81e4847fe9da31a47d4e2b72d4
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-01-29 23:42:21 -0300

    Update Config

--- src/app/purchase/purchase.service.ts
+++ src/app/purchase/purchase.service.ts
@@ -12,55 +12,47 @@ import { Injectable } from '@angular/core'
 
 export class PurchaseService {
 
-    private purchase: any
-    private cart = []
-    private db = null
     private uid: String
-    private count = 0
+    private purchase: any
+    private cart: any
+    private db = null
     private responses = []
 
     public constructor(purchase: FirebasePurchaseService) {
         this.purchase = purchase
     }
 
-    public add(product: any) {
-        product.pid = this.generatePId()
-        this.cart.push(product)
-    }
-
-    public remove(pid: String) {
-        this.cart.forEach((product, index)=> {
-            if (product.pid == pid) {
-                delete this.cart[index]
-            }
-        })
-    }
-
-    private generatePId() {
-        return (this.count + 1)
-    }
-
     public setUid(uid: String) {
         this.uid = uid
     }
 
-    public save(callback: any) {
+    public setCart(purchase: any) {
+        this.cart = []
+        this.cart = this.cart.concat(purchase)
+    }
+
+    public makeTransaction(callback: any) {
+        
         var that = this
         var total = that.cart.length
         var counter = 0
-
-        that.purchase.uid = that.uid
+        
+        that.purchase.setUid(this.uid)
 
         that.cart.forEach((product)=> {
-            delete product.pid
+
             that.purchase.save(product, (response)=>{
+
                 that.responses.push(response)
+                counter = (counter + 1)
+                
                 if (counter == total) {
                     callback(that.responses)
-                } else {
-                    counter = (counter + 1)
                 }
+
+                console.log("SAVED PRODUCT: ", response, "COUNTER: ", counter, "TOTAL: ", total)
             })
+
         })
     }
 }
