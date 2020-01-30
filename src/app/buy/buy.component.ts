733bb616e8998b90c9baa971004feaa4a15ec8a1
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-01-30 02:32:10 -0300

    Init Layout

--- src/app/buy/buy.component.ts
+++ src/app/buy/buy.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseLoginService } from './../firebase/firebase.login.service'
+import { PurchaseService } from './../purchase/purchase.service'
+import { TicketComponent } from './../ticket/ticket.component'
 
 @Component({
-  selector: 'app-buy',
-  templateUrl: './buy.component.html',
-  styleUrls: ['./buy.component.css']
+    selector: 'app-buy',
+    templateUrl: './buy.component.html',
+    styleUrls: ['./buy.component.css']
 })
+
 export class BuyComponent implements OnInit {
 
-  constructor() { }
+    private uid: any
+    private login: any
+    private purchase: any
 
-  ngOnInit() {
-  }
+    public list: any
+    public control = {
+        bar: false
+    }
+
+    public constructor(login: FirebaseLoginService, purchase: PurchaseService) { 
+        this.login = login
+        this.purchase = purchase
+    }
+
+    ngOnInit() {
+        var that = this
+        that.login.scope((user)=> { if (null !== user) { that.uid = user.uid } })
+        that.login.check(null, 'login')
+        that.list = []
+
+        that.load()
+    }
+
+    public load() {
+        var that = this
+
+        that.control.bar = true
+
+        setTimeout(()=> {
+            that.purchase.setUid(that.uid)
+            that.purchase.getList((response)=> {
+                that.control.bar = false
+                if ("200" == response.code) {
+                    that.list = that.list.concat(response.list)
+                }
+            })
+        }, 1500)
+    }
 
 }
