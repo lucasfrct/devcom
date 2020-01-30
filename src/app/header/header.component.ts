50eb74ce25f911ff67119218f5205e9f82b958cb
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-01-30 02:31:34 -0300

    Add Acess Controled
    Adicionado acesso controlado

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseLoginService } from './../firebase/firebase.login.service'
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    private login: any
 
-  ngOnInit() {
-  }
+    constructor(login: FirebaseLoginService) { 
+        this.login = login
+    }
+
+    ngOnInit() {
+        this.login.check()
+    }
+
+    public logOut() {
+        this.login.denied()
+    }
 
 }
