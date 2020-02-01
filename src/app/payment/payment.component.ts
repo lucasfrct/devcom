b7ae9f0791fe951e943d4bc1bf1dd928927f258a
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-02-01 20:04:21 -0300

    Add Module de Paymanet

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+    selector: 'app-payment',
+    templateUrl: './payment.component.html',
+    styleUrls: ['./payment.component.css']
 })
+
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+    control = {
+        bank: false,
+        paypal: false,
+        unchecked: false,
+        selected: ""
+    }
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    public toggleBank() {
+        this.control.bank = !this.control.bank
+        this.unchecked(this.control.bank)
+        if (this.control.bank) {
+            this.control.paypal = false
+        }
+
+        this.selected()
+
+    }
+
+    public togglePaypal() {
+        this.control.paypal = !this.control.paypal
+        this.unchecked(this.control.paypal)
+
+        if (this.control.paypal) {
+            this.control.bank = false
+        }
+
+        this.selected()
+    }
+
+    public unchecked(state: Boolean) {
+        return this.control.unchecked = (state) ? true : false
+    }
+
+    private selected() {
+        if (this.control.bank && !this.control.paypal) {
+            this.control.selected = "bank"
+        }
+
+        if(!this.control.bank && this.control.paypal) {
+            this.control.selected = "paypal"
+        }
+
+        if(!this.control.bank && !this.control.paypal) {
+            this.control.selected = ""
+        }
+
+        return this.selected
+    }
+
+    public type() {
+        return this.control.selected
+    }
+
+    public states() {
+        return this.control
+    }
+
 
 }
