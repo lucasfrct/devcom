81c2e3c9c1971c79bea7833231a970db75336e2a
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-02-01 00:41:21 -0300

    Update Shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -26,8 +26,10 @@ export class ShopComponent implements OnInit {
     public purchase = []
 
     public control = {
-        modal: null,
-        modalPurchase: null,
+        modal: {
+            purchase: false,
+            pay: false,
+        },
         method: false,
         total: 0,  
     }
@@ -46,16 +48,16 @@ export class ShopComponent implements OnInit {
         this.event = Event.currentEvent
         this.ticket = Ticket.currentTicket
 
-        this.user = { 
+        /*this.user = { 
             name: "Cliente", 
             surname: 'c',
             telephone: '11111111',
             email:"cliente@cliente", 
             bi: "1234123423",
-        }
+        }*/
     }
     
-    ngOnInit() { 
+    public ngOnInit() { 
         var that = this
 
         that.Login.check(null, 'login')
@@ -69,13 +71,13 @@ export class ShopComponent implements OnInit {
         })
     }
 
-    private loadEvent(callback: any = null){
+    private loadEvent(callback: any = null) {
         this.Event.setUid(this.uid)
+        this.Event.Subscribe(callback)
+
         this.Event.read((response)=> { 
             this.event = this.Event.extend(this.event, response.events[0]) 
-            if (callback) {
-                callback(this.event)
-            }
+            this.Event.NotifyAll(this.event)
         })
     }
     
@@ -102,18 +104,21 @@ export class ShopComponent implements OnInit {
 
     public addTicket(ticket) {
 
+        console.log("TK", ticket)
+
         var that = this
         
         if (that.Ticket.validate(ticket).check) {
             
-            that.control.modal = true
+            that.control.modal.purchase = true
             
             ticket.uid = that.uid
             ticket.eid = that.event.id
+            ticket.event = that.Ticket.extend(ticket.event, that.event)
 
             that.Event.getTicketEdition(ticket.seat.type, (edition) => {
 
-                that.control.modal = false
+                that.control.modal.purchase = false
 
                 ticket.edition = that.Purchase.extend(ticket.edition, edition)
                 
@@ -127,25 +132,23 @@ export class ShopComponent implements OnInit {
     }
 
     public finallyPurchase() {
-        console.log("FINALLY INIT")
         
        this.Purchase.setUid(this.uid)
        this.Purchase.setUser(this.user)
 
         if (this.Purchase.valid().check) {
             
-            this.control.modalPurchase = true;
+            this.control.modal.pay = true;
             
             this.Purchase.transaction((response)=> {
 
-                this.control.modalPurchase = false;
+                this.control.modal.pay = false;
                 this.notify("Ingressos reservados com sucesso")
                 this.Login.redirect("payment")
+
             })
         }
 
-
-        
     }
 
     private notify(message: String, time = 4000) {
