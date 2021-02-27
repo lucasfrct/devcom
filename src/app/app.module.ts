9a2c1b915b76cc21623f411675cadbc5da752776
lucasfrct <lucasfrct@users.noreply.github.com>
2021-02-27 10:56:56 -0300

    Add Materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//ANGULAR 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
+// Directive
+//import { SidenavDirective } from './sidenav/sidenav.directive'
+
+// components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent]
 })
 export class AppModule { }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseLoginService } from './../firebase/firebase.login.service'
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+    
+    private Login: any
+    public control = {
+        state: false
+    }
+
+    constructor(Login: FirebaseLoginService) { 
+        this.Login = Login
+    }
+
+    ngOnInit(): void {
+        this.Login.check((state: any)=> {
+            this.control.state = state
+        })
+    }
+
+    
+    public logOut() {
+        this.Login.denied()
+    }
+
+}
